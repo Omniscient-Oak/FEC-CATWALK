6e118fb7b67214a957a29d2c127aed386870d5c9
MichaelWu <vmichael.wu@gmail.com>
2021-12-21 12:00:52 -0800

    First Commit: Webpack

---
+++ client/src/component/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return(
+      <div>this is an app!</div>
+    )
+  }
+}
+
+export default App;
