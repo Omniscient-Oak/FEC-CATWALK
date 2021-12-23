77f695ca216e35dffff0175b6d48525b6673fbd3
MichaelWu <vmichael.wu@gmail.com>
2021-12-23 10:31:41 -0800

    Feat: Fixed jest test

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -5,7 +5,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { screen } from '@testing-library/dom';
-import renderer from 'react-test-renderer';
 import App from '../client/src/component/App';
 
 test('renders a message', () => {
