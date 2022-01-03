cebae8879da541844cb242410fdc95a6eec6403a
ericjlie <ericjlie@gmail.com>
2022-01-02 20:12:28 -0800

    fix:Fixed the QA server routes

--- server/controllers/qa.js
+++ server/controllers/qa.js
@@ -24,6 +24,7 @@ module.exports = {
         name: req.body.name,
         email: req.body.email,
         product_id: req.body.product_id,
+        photos: req.body.photos,
       },
       {
         headers: {
@@ -66,7 +67,7 @@ module.exports = {
           Authorization: auth,
         },
         params: {
-          product_id: req.query.product_id,
+          question_id: req.query.question_id,
           count: req.query.count,
           page: req.query.page,
         },
@@ -79,7 +80,8 @@ module.exports = {
         body: req.body.body,
         name: req.body.name,
         email: req.body.email,
-        product_id: req.body.product_id,
+        question_id: req.body.question_id,
+        photos: req.body.photos,
       },
       {
         headers: {
